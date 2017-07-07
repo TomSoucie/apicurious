daa087e387724578624a6dfdb1560b8875675c5e
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-07 08:08:35 -0600

    add initial routes for home#index, /auth/github and /auth/github/callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get '/auth/github', as: :github_login
+  get '/auth/github/callback', to: 'session#create' 
 end
---
+++ spec/features/user_can_authenticate_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+=begin
+As a registered user
+When I visit the home page
+I should see a link to authenticate
+And when I click on the link
+The current_path should be "/auth/github"
+And when I click on "authenticate"
+I should see a success message
+And the user dashboard should be visible
+=end
+
+describe "A user can be authenticated" do
+  context "a registered user" do
+
+  end
+end
